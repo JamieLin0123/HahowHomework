757d90cc87fc15ae29e07f72cb8509244a1a5c95
林宜潔 <>
2019-05-23 18:06:38 +0800

    homeWork2 finish

--- app/src/main/java/com/jamie/hahowhomework/MainActivity.java
+++ app/src/main/java/com/jamie/hahowhomework/MainActivity.java
@@ -1,13 +1,45 @@
 package com.jamie.hahowhomework;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etNtd;
+    private Button btnGo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViews();
+    }
+
+    private void findViews() {
+        etNtd = findViewById(R.id.ntd);
+        btnGo = findViewById(R.id.btn_go);
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(etNtd.getText().toString().equals("")){
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setTitle("Problem")
+                            .setMessage("Please enter your NTD amout")
+                            .setPositiveButton("OK", null)
+                            .show();
+                }else{
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setTitle("Result")
+                            .setMessage("USD is " + Float.parseFloat(etNtd.getText().toString())/30.9)
+                            .setPositiveButton("OK", null)
+                            .show();
+                }
+            }
+        });
     }
 }
